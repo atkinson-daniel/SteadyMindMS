bd7e50775b7fbb78bd678f12945e1348119891d5
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-03 14:36:05 -0600

    create happy end point to return happy videos

--- lib/video_microservice.rb
+++ lib/video_microservice.rb
@@ -14,3 +14,13 @@ get '/sad' do
 
   VideoSerializer.new(videos).serialized_json
 end
+
+get '/happy' do
+  playlist_items = YouTubeService.get_playlist_items('PL2gVg27Uw-kcsCzSgn8Hufs4QKWdARfuv')[:items]
+
+  videos = playlist_items.map.with_index(1) do |item, index|
+    Video.new(item, index)
+  end
+
+  VideoSerializer.new(videos).serialized_json
+end
