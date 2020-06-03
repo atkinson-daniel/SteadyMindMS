54baf0a5f87de3e43c665f70ea5e64d35a3a0b15
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-03 14:35:54 -0600

    Test it can return happy videos from playlist

--- spec/requests/video_microservice_spec.rb
+++ spec/requests/video_microservice_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe 'Video Microservice' do
     expect(first_video[:attributes]).to have_key :thumbnail_url
     expect(first_video[:attributes]).to have_key :video_id
   end
+
+  it 'can return happy videos' do
+    get '/happy'
+
+    videos = JSON.parse(last_response.body, symbolize_names: true)
+    first_video = videos[:data].first
+
+    expect(last_response).to be_ok
+    expect(first_video[:attributes]).to have_key :category
+    expect(first_video[:attributes]).to have_key :title
+    expect(first_video[:attributes]).to have_key :thumbnail_url
+    expect(first_video[:attributes]).to have_key :video_id
+  end
 end
