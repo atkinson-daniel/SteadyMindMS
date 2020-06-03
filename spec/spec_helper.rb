94924cbef47548437c3186da560eed3919d3349b
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-03 14:28:14 -0600

    Add RSpec and update spec_helper with config data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,15 @@ require 'rspec'
 
 ENV['RACK_ENV'] = 'test'
 
-require 'bundler'
-Bundler.require(:default, :test)
-
-# require File.expand_path '../../lib/video_microservice.rb', __FILE__
+require File.expand_path '../../lib/video_microservice.rb', __FILE__
+require File.expand_path '../../lib/you_tube_service.rb', __FILE__
+require File.expand_path '../../lib/video.rb', __FILE__
+require File.expand_path '../../lib/video_serializer.rb', __FILE__
 
 module RSpecMixin
   include Rack::Test::Methods
-  def app() described_class end
+  def app() Sinatra::Application end
 end
 
+# For RSpec 2.x and 3.x
 RSpec.configure { |c| c.include RSpecMixin }
