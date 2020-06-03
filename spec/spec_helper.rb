76f544d85ccc80c391979eef150c40c02b332c58
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-03 15:02:57 -0600

    Configure VCR and webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rack/test'
 require 'rspec'
+require 'vcr'
+require 'webmock'
 
 ENV['RACK_ENV'] = 'test'
 
@@ -15,3 +17,10 @@ end
 
 # For RSpec 2.x and 3.x
 RSpec.configure { |c| c.include RSpecMixin }
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<YOUTUBE-API-KEY>') { ENV['YOUTUBE-API-KEY'] }
+  config.configure_rspec_metadata!
+end
