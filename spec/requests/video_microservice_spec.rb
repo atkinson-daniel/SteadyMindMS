006903f2ba4dfe12a0e0016d14e8f7b185f253ed
Steven Meyers <sjjmeyers@gmail.com>
2020-06-03 20:10:58 -0600

    Add video prefix to all routes and all video endpoint

--- spec/requests/video_microservice_spec.rb
+++ spec/requests/video_microservice_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe 'Video Microservice' do
   it 'can return sad videos', :vcr do
-    get 'api/v1/sad'
+    get 'api/v1/videos/sad'
 
     videos = JSON.parse(last_response.body, symbolize_names: true)
     first_video = videos[:data].first
@@ -15,7 +15,7 @@ RSpec.describe 'Video Microservice' do
   end
 
   it 'can return happy videos', :vcr do
-    get 'api/v1/happy'
+    get 'api/v1/videos/happy'
 
     videos = JSON.parse(last_response.body, symbolize_names: true)
     first_video = videos[:data].first
@@ -28,7 +28,20 @@ RSpec.describe 'Video Microservice' do
   end
 
   it 'can return neutral videos', :vcr do
-    get 'api/v1/neutral'
+    get 'api/v1/videos/neutral'
+
+    videos = JSON.parse(last_response.body, symbolize_names: true)
+    first_video = videos[:data].first
+
+    expect(last_response).to be_ok
+    expect(first_video[:attributes]).to have_key :category
+    expect(first_video[:attributes]).to have_key :title
+    expect(first_video[:attributes]).to have_key :thumbnail_url
+    expect(first_video[:attributes]).to have_key :video_id
+  end
+
+  it 'can return all videos', :vcr do
+    get 'api/v1/videos'
 
     videos = JSON.parse(last_response.body, symbolize_names: true)
     first_video = videos[:data].first
